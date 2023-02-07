594b9fe991240104bef55837eb2c6c5bf061c858
moncef aissaoui <108152453+modecode22@users.noreply.github.com>
2023-02-07 22:00:24 +0100

    gg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,16 @@
 import LogInBtn from "@/components/LogInBtn";
-import axios, { AxiosResponse } from 'axios'
+import "@/app/globals.css"
 
 type  Data = {
   name: string
   description: string
 }
-async function getData() {
-  const res = await fetch("http://127.0.0.1:3000/api/data");
-
-  if (!res.ok) {
-    throw new Error('Failed to fetch data');
-  }
-
-  return res.json();
-}
-
-
 
 export default async function Home() {
 
-  const data= await  getData() as Data[]
-   
-  
-  
+  const data = await(
+    await fetch(`${process.env.NEXT_API_URL}/api/data`)
+  ).json() as Data[];
   return (
     <>
       <header className="bg-stone-900 h-16 px-16 flex justify-center items-center font-bold text-4xl">
@@ -30,7 +18,7 @@ export default async function Home() {
       </header>
       <LogInBtn/>
       <main className="px-10 py-5 grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3  lg:grid-cols-4  gap-5">
-        {data? data?.map((e) => (
+        {data?.map((e) => (
           <div
             className="group  hover:border-red-700 p-5 rounded-lg bg-stone-900 border border-red-900 shadow-sm hover:shadow-md duration-100 cursor-pointer shadow-stone-900"
             key={Math.random()}
@@ -40,7 +28,7 @@ export default async function Home() {
               {e.description}
             </h3>
           </div>
-        )):<div>drfg</div>}
+        ))}
       </main>
     </>
   );
